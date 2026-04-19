169caaad7ebd404cb9fffd6e1a3fdbf88047bc75
heneblaise <heneblaise21@gmail.com>
2026-04-19 20:16:19 +0100

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,29 @@
+
+// CENTRAL QUOTES STORAGE (EASY TO EDIT LATER)
+const QUOTES = {
+  motivation:[
+    "Stay strong.",
+    "Never give up.",
+    "Push your limits."
+  ],
+  love:[
+    "Love conquers all.",
+    "You are enough.",
+    "Love starts within you."
+  ],
+  success:[
+    "Success is earned, not given.",
+    "Work hard in silence.",
+    "Dream big, act bigger."
+  ],
+  life:[
+    "Life is what you make it.",
+    "Enjoy every moment.",
+    "Live with purpose."
+  ],
+  happiness:[
+    "Happiness is a choice.",
+    "Smile more often.",
+    "Peace begins with you."
+  ]
+};
